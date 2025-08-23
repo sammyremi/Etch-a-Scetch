1d99634c48091623453ffe766696a4c87f8863ed
sammyremi <adebayorsamuelas@gmail.com>
2025-08-23 01:53:17 +0100

    feat: add heaight and border to div

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container")
+
+for (x = 0; x < 16; x++){
+    div = document.createElement("div")
+    div.classList.add("newDivs")
+    container.appendChild(div)
+}
+
+
+
