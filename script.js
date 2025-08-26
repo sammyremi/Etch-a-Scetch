95b6b3fe80ac82b6962e8c1886869436f60ccd53
sammyremi <adebayorsamuelas@gmail.com>
2025-08-26 04:16:20 +0100

    feat: add multi colors to draw board

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let btn = document.querySelector(".btn")
+drawBoard(16)
+pixelDraw()
+
 function divCreator(num){
     div = document.createElement("div")
     for (x = 0; x < num; x++){
@@ -15,13 +19,44 @@ function drawBoard(size){
     }
 }
 
-drawBoard(60)
+function pixelDraw(){
+    let pixels = document.querySelectorAll(".inDiv")
 
-let pixels = document.querySelectorAll(".inDiv")
-console.log(pixels)
-
-for (const pixel of pixels){
+    
+    for (const pixel of pixels){
         pixel.addEventListener('mousemove', (e) => {
-        pixel.style.backgroundColor = "green"
-    })
+            let color = selectColor()
+            pixel.style.backgroundColor = color
+        })
+    }
+}
+
+function removeDiv(){
+    divs = document.querySelectorAll(".inDiv")
+    for (let div of divs){
+        div.remove()
+    }
+}
+
+
+btn.addEventListener("click", () => {
+    let size = prompt("Enter Number 1 - 100")
+
+    if (+size < 1 || +size > 100){
+        alert("Number must be between 1 - 100")
+        return
+    }
+
+    removeDiv()
+
+    drawBoard(+size)
+    pixelDraw()
+})
+
+
+function selectColor(){
+
+    let colors = ["green", "yellow", "red", "blue", "black", "purple"]
+    let color = Math.random() * 6
+    return colors[Math.floor(color)];
 }
